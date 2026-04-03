c41d5ecae1b54a07b1defc15a52666c42aa7e772
surendra <sk9571270392@gmail.com>
2026-04-03 11:39:07 +0530

    fix app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask
 import os
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "App is running!"
+
+@app.route("/hackaicon_ethiack_1337_lmao")
+def hack():
+    return "CTF endpoint working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
