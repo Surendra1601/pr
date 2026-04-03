e6fd2546a2d87d829df98c0d14d3093d443f4fa0
surendra <sk9571270392@gmail.com>
2026-04-03 12:04:56 +0530

    fix app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "App is running!"
-
 @app.route("/hackaicon_ethiack_1337_lmao")
 def hack():
-    return "CTF endpoint working!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    return f"""
+    Received request from agent 🚀
+    
+    Headers:
+    {dict(request.headers)}
+    
+    Args:
+    {request.args}
+    """
